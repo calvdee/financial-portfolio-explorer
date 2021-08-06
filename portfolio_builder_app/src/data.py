c9cc90c9e6773318090f6afb517d0252d7dddaea
Calvin De Lima <calvindlm@gmail.com>
2021-08-06 15:17:00 -0400

    Got rid of extras

--- portfolio_builder_app/src/data.py
+++
@@ -1,9 +0,0 @@
-from portfolio_builder.src.assets import make_monthly_asset_data
-from portfolio_builder.src.portfolios import Portfolio
-
-def get_asset_returns_data(portfolio: list):
-  dfs = make_monthly_asset_data(portfolio)
-  return dfs
-
-def get_portfolio(symbols: str, weights = None):
-  return Portfolio(symbols, weights)
--- portfolio_design/app.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-from .src.data import get_monthly_market_data
